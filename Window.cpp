acead0157465575679ff6efcc650c1ce64a3a061
Ileana Martinez <ilimartinez10053@gmail.com>
2025-04-24 23:11:52 -0600

    Nombre de la ventana

--- Window.cpp
+++ Window.cpp
@@ -51,7 +51,7 @@ int Window::Initialise()
 	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 
 	//CREAR VENTANA
-	mainWindow = glfwCreateWindow(width, height, "Practica 08. Iluminacion 2", NULL, NULL);
+	mainWindow = glfwCreateWindow(width, height, "Proyecto final", NULL, NULL);
 
 	if (!mainWindow)
 	{
